9c5aa15b16bb2eab816df9076910daba5465022a
MKHLimkilde <168226952+MKHLimkilde@users.noreply.github.com>
2025-05-06 17:15:25 -0700

    Create DataLoader.cpp

---
+++ DataLoader.cpp
@@ -0,0 +1,21 @@
+/* 
+    Hauen-Limkilde, Marcus
+    DataLoader.cpp
+*/
+#include "DataLoader.h"
+#include "WorkshopList.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+class DataLoader
+{
+    public: 
+        static void DataLoader::loadWorkshops(WorkshopList workshopList, string filename)
+        {
+            ifstream file(filename);
+            
+        }
+}
