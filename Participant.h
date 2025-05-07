f504ffef1a6108b608616d27b3a54102ca4eaca5
NoahW00 <158109662+NoahW00@users.noreply.github.com>
2025-05-06 17:06:02 -0700

    Add files via upload

---
+++ Participant.h
@@ -0,0 +1,27 @@
+/*
+    Williams, Noah (Team Leader)
+
+    Spring 2025
+    CS A250 - C++ 2
+
+    Participant
+*/
+
+#include <string>
+
+using namespace std;
+
+class Participant
+{
+    public:
+        Participant(int id, string theFirstName, string theLastName);
+        int getID() const;
+        string GetFirstName() const;
+        string GetLastName() const;
+        bool operator<(Participant participant);
+
+    private:
+        int id;
+        string firstName;
+        string lastName;
+};
