d4495395b32529a5ad0a69acea369cc158e23d1e
Marcus Hauen-Limkilde <168226952+MKHLimkilde@users.noreply.github.com>
2025-05-10 10:34:21 -0700

    Update DataLoader.cpp

--- DataLoader.cpp
+++ DataLoader.cpp
@@ -3,19 +3,36 @@
     DataLoader.cpp
 */
 #include "DataLoader.h"
-#include "WorkshopList.h"
-#include <string>
 #include <fstream>
-#include <iostream>
+#include <string>
 
 using namespace std;
 
-class DataLoader
+void DataLoader::loadWorkshops(WorkshopList workshopList, string filename)
 {
-    public: 
-        static void DataLoader::loadWorkshops(WorkshopList workshopList, string filename)
-        {
-            ifstream file(filename);
-            
-        }
+    ifstream file(filename);
+
+    if (!file.is_open())  { return; }
+
+    string line;
+    while(getline(file,line))
+    {
+        stringstreams ss(line);
+        int number, capacity, hour;
+        double price;
+        string title;
+
+        ss >> number;
+        getline(ss, title, '|');
+        ss >> hour;
+        ss.ignore(1);
+        ss >> capacity;
+        ss.ignore(1);
+        ss >> price;
+
+        Workshop workshop(number, title, hours, capacity, price);
+        workshopList.addWorkshop(workshop);
+    }
+    
+    file.close();
 }
