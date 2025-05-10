18cd2932e007d1c5fa69082695b3b84ecc32b499
r-ayala <rayala29@student.cccd.edu>
2025-05-09 21:08:33 -0700

    Update WorkshopList.cpp

--- WorkshopList.cpp
+++ WorkshopList.cpp
@@ -9,53 +9,50 @@
 
 #include "WorkshopList.h"
 
-class WorkshopList
+void WorkshopList::addWorkshop(const Workshop& workshop)
 {
+	workshops.insert(workshop);
+};
 
-	void WorkshopList::addWorkshop(Workshop workshop)
-	{
-		workshops.insert(workshop);
-	};
+int WorkshopList::getNumber(const Workshop& workshop) const
+{
+	return workshop.theNumber;
+};
 
-	int WorkshopList::getNumber(Workshop workshop)
-	{
-		return workshop.theNumber;
-	};
+std::string WorkshopList::getTitle(int workshopNo) const
+{
+	return workshop.theTitle;
+};
 
-	std::string WorkshopList::getTitle(int workshopNo)
-	{
-		return workshop.theTitle;
-	};
+int WorkshopList::getHours(int workshopNo) const
+{
+	auto workshopIt = findByNumber(workshopNo);
+	return workshopIt->theHours;
+};
 
-	int WorkshopList::getHours(int workshopNo)
-	{
-		return workshop.theHours;
-	};
+int WorkshopList::getCapacity(int workshopNo) const
+{
+	auto workshopIt = findByNumber(workshopNo);
+	return workshopIt->theCapacity;
+};
 
-	int WorkshopList::getCapacity(int workshopNo)
-	{
-		return workshopNo.theCapacity;
-	};
+double WorkshopList::getPrice(double workshopNo) const
+{
+	auto workshopIt = findByNumber(workshopNo);
+	return workshopIt->thePrice;
+};
 
-	double WorkshopList::getPrice(double workshopNo)
-	{
-		return workshopNo.thePrice;
-	};
+bool WorkshopList::isEmpty()
+{
+	return workshops.empty();
+};
 
-	bool WorkshopList::isEmpty()
-	{
-		return workshops.empty();
-	};
+void WorkshopList::clearList()
+{
+	workshops.clear();
+};
 
-	void WorkshopList::clearList()
-	{
-		workshops.clear();
-	};
-
-	set<Workshop>::iterator WorkshopList::findByNumber(int workshopNo)
-	{
-		return workshops.find(Workshop{workshopNo, "", 0, 0, 0.0});
-	};
-
-
+set<Workshop>::iterator WorkshopList::findByNumber(int workshopNo)
+{
+	return workshops.find(Workshop{workshopNo, "", 0, 0, 0.0});
 };
