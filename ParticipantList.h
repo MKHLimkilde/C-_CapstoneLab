7aa2f7117fb4930df093bb5b53805ecb6c387580
r-ayala <rayala29@student.cccd.edu>
2025-05-09 20:50:10 -0700

    Update ParticipantList.h

--- ParticipantList.h
+++ ParticipantList.h
@@ -1,27 +1,35 @@
+/*
+    Ayala, Ruben
+
+    Spring 2025
+    CS A250 - C++ 2
+    
+    ParticipantList.h
+*/
+
 #ifndef PARTICIPANTLIST_H
 #define PARTICIPANTLIST_H
+
 #include "Participant.h"
-#include <iostream>
+#include "Workshop.h"
+#include <map>
+#include <string>
 #include <vector> 
+
 class ParticipantList
 {
 	public:
-
-		void addParticipant(Participant participant);
-		void addWorkshopToParticipant(Participant participant, Workshop workshop);
-		int getID(Participant participant);
-		std::string getFirstName(int participantID);
-		std::string getLastName(int participantID);
-		vector<Workshop> getWorkshops(int participantID);
+		void addParticipant(const Participant& participant);
+		void addWorkshopToParticipant(const Participant& participant, const Workshop& workshop);
+		int getID(Participant participant) const;
+		std::string getFirstName(int participantID) const;
+		std::string getLastName(int participantID) const;
+		std::vector<Workshop> getWorkshops(int participantID) const;
 		void clearList();
 
 	private:
-		
-		findByID(int participantID) :
-			map<Participant, vector<Workshop>>::const_iterator
-
-
+		std::map<Participant, std::vector<Workshop>> participantList;
+		std::map<Participant, std::vector<Workshop>>::const_iterator findByID(int participantID);
 };
-
 #endif
 
