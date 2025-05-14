438726f18cebfe1c276d785ceee039b74c8d58c0
r-ayala <rayala29@student.cccd.edu>
2025-05-14 13:09:08 -0700

    Update ParticipantList.cpp

--- ParticipantList.cpp
+++ ParticipantList.cpp
@@ -3,72 +3,84 @@
 
     Spring 2025
     CS A250 - C++ 2
+
     ParticipantList.cpp
 */
 
+
 #include "ParticipantList.h"
+#include <algorithm> // for std::find_if
 
 void ParticipantList::addParticipant(const Participant& participant)
 {
-	participantMap[participant] = {};
+    participantList.insert({ participant, {} });
 }
 
 void ParticipantList::addWorkshopToParticipant(const Participant& participant, const Workshop& workshop)
 {
-	participantMap[participant].push_back(workshop); 
+    participantList[participant].push_back(workshop);
 }
 
 int ParticipantList::getID(const Participant& participant) const
 {
-	return participant.id;
+    return participant.getID();
 }
 
 std::string ParticipantList::getFirstName(int participantID) const
 {
-	for (const auto& [participant, workshops] : participantList)
-	{
-		if (participant.id == participantID)
-		{
-			return participant.firstName;
-		}
-	}
+    auto it = findByID(participantID);
+    return it->first.getFirstName();
 }
 
 std::string ParticipantList::getLastName(int participantID) const
 {
-	for (const auto& [participant, workshops] : participantList)
-	{
-		if (participant.id == participantID)
-		{
-			return participant.lastName;
-		}
-	}
+    auto it = findByID(participantID);
+    return it->first.getLastName();
 }
 
 std::vector<Workshop> ParticipantList::getWorkshops(int participantID) const
 {
-	for (const auto& [participant, workshops] : participantList)
-	{
-		if (participant.id == participantID)
-		{
-			return workshops;
-		}
-	}
+    auto it = findByID(participantID);
+    return it->second;
 }
 
-void clearList()
+bool ParticipantList::isEmpty() const
 {
-	participantList.clear();
+    return participantList.empty();
 }
 
-
-std::map<Participant, std::vector<Workshop>>::const_iterator ParticipantList::findByID(int participantID) const
+void ParticipantList::clearList()
 {
-	for (auto mapIt = participantList.begin(); it != participantList.end(); ++mapIt)
-	{
-		if (mapIt->first.id == participantID)
-		{
-			return mapIt;
-		}
-	}
+    participantList.clear();
+}
+
+std::map<Participant, std::vector<Workshop>>::const_iterator
+ParticipantList::findByID(int participantID) const
+{
+    return std::find_if(participantList.begin(), participantList.end(),
+        [participantID](const std::pair<Participant, std::vector<Workshop>>& entry)
+        {
+            return entry.first.getID() == participantID;
+        });
+}
+
+const Participant& ParticipantList::getParticipant(int participantID) const // new funciton for part B
+{
+    auto participantIt = findByID(participantID);
+    return it->first;
+}
+
+void ParticipantList::cancelWorkshop(int participantID, int workshopNo) // new function for part B
+{
+    auto participantIt = findByID(participantID);
+
+    auto& workshops = participantIt->second;
+
+    auto workshopIt = std::remove_if(workshops.begin(), workshops.end(),
+                        [workshopNo](const Workshop& workshop)
+                        {
+                            return workshop.getWorkshopNumber() == workshopNo;
+                        }
+
+    workshops.erase(workshopIt, workshops.end());
 }
