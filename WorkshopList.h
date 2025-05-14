22aee1264cd61d93ceb37ac7ba577bca7c16fdf7
r-ayala <rayala29@student.cccd.edu>
2025-05-14 13:05:24 -0700

    Update WorkshopList.h

--- WorkshopList.h
+++ WorkshopList.h
@@ -4,31 +4,31 @@
 	Spring 2025
 	CS A250 - C++ 2
 
-	Participant
+	WorkshopList.h
 */
 
 #ifndef WORKSHOPLIST_H
 #define WORKSHOPLIST_H
 
-#include <string>
 #include <set>
-#include <iostream>
+#include "Workshop.h"  // Make sure this is included here
 
-class WorkshopList
-{
-	public:
+class WorkshopList {
+public:
+    void addWorkshop(const Workshop& workshop);
+    int getNumber(const Workshop& workshop) const;
+    std::string getTitle(int workshopNo) const;
+    int getHours(int workshopNo) const;
+    int getCapacity(int workshopNo) const;
+    double getPrice(int workshopNo) const;
+    const Workshop& getWorkshop(int workshopNo) const; // new function for part B
+    const std::set<Workshop>& getAllWorkshops() const; // new function for part B
+    bool isEmpty() const;
+    void clearList();
 
-		void addWorkshop(const Workshop& workshop);
-		int getNumber(const Workshop& workshop) const;
-		std::string getTitle(int workshopNo) const;
-		int getHours(int workshopNo) const;
-		int getCapacity(int workshopNo) const;
-		int getPrice(int workshopNo) const;
-		bool isEmpty() const;
-		void clearList();
-
-	private:
-		std::set<Workshop> workshops;
-		std::set<Workshop>::iterator findByNumber(int workshopNo);
+private:
+    std::set<Workshop> workshopList;
+    std::set<Workshop>::const_iterator findByNumber(int workshopNo) const;
 };
+
 #endif
